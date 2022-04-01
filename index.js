8f86dc0c3aa51f7566de0ad2025c32352b104d44
Judah Kerr <98183571+JudahKerr@users.noreply.github.com>
2022-04-01 13:32:30 -0700

    Finished Project!
    Got it working how I wanted! 
    I'm sure I could have used more concise code to get the same result but I'm happy it's working correctly.

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
+//Declaring DOM variables
+let tip5 = $(".5");
+let tip10 = $(".10");
+let tip15 = $(".15");
+let tip25 = $(".25");
+let tip50 = $(".50");
+let tipcustom = $("#custom");
+let tips = [tip5, tip10, tip15, tip25, tip50, tipcustom];
+let resetBtn = $(".reset");
+let buttons = document.getElementsByClassName("box");
+let customButton = $(".custom-button");
+let customPercent = 0;
 
+//Custom Percent DIV Logic
+tipcustom.on("click", function() {
+  document.querySelector(".custom").style.visibility = "visible"
+})
+customButton.on("click", function() {
+  document.querySelector(".custom").style.visibility = "hidden"
+  customPercent = document.querySelector(".custom-input").value;
+  customTip(customPercent);
+})
+
+//Reset Button Logic
+resetBtn.on("click", function() {
+  $("#tip-output-display").text("$0.00");
+  $("#total-amount-display").text("$0.00");
+  document.getElementById("bill").value = "";
+  document.querySelector(".people-input").value = "1";
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].classList.contains("clicked") === true) {
+      buttons[i].classList.remove("clicked");
+    }
+
+
+  }
+})
+
+//Calculating Custom Tip if selected
+function customTip(num) {
+  let bill = document.getElementById("bill").value;
+  let peopleAmount = document.querySelector(".people-input").value;
+  let tipPercentSelected = num / 100;
+  let tipAmount = (bill * tipPercentSelected) / peopleAmount;
+  tipAmount = Math.round((tipAmount + Number.EPSILON) * 100) / 100;
+  let totalAmount = (parseInt(bill) + (tipAmount * parseInt(peopleAmount))) / parseInt(peopleAmount);
+  totalAmount = Math.round((totalAmount + Number.EPSILON) * 100) / 100;
+
+  $("#tip-output-display").text("$" + tipAmount);
+  if (bill == 0) {
+    return;
+  } else {
+    $("#total-amount-display").text("$" + totalAmount);
+  }
+
+}
+
+
+//Main Logic for Tip Buttons
+for (let i = 0; i < tips.length; i++) {
+  tips[i].on("click", function() {
+    let bill = document.getElementById("bill").value;
+    let peopleAmount = document.querySelector(".people-input").value;
+    let tipPercentSelected = this.value / 100;
+
+    if (customPercent > 0) {
+      tipPercentSelected = customPercent / 100;
+    }
+
+    let tipAmount = (bill * tipPercentSelected) / peopleAmount;
+    tipAmount = Math.round((tipAmount + Number.EPSILON) * 100) / 100;
+    let totalAmount = (parseInt(bill) + (tipAmount * parseInt(peopleAmount))) / parseInt(peopleAmount);
+    totalAmount = Math.round((totalAmount + Number.EPSILON) * 100) / 100;
+
+    $("#tip-output-display").text("$" + tipAmount);
+    if (bill == 0) {
+      return;
+    } else {
+      $("#total-amount-display").text("$" + totalAmount);
+    }
+    toggleClass(this);
+  })
+}
+
+//Toggle between which button is selected
+function toggleClass(el) {
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].classList.contains("clicked") === true) {
+      buttons[i].classList.remove("clicked");
+    }
+  }
+  el.classList.toggle("clicked");
+}
